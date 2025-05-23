0a302bdfc19da4adcf57b555acd5d204bf2af622
Byreddy Rajesh <rajeshbyreddy@Byreddys-MacBook-Air.local>
2025-05-23 22:49:49 +0530

    updated vercel.json file

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const dotenv = require('dotenv')
-dotenv.config()
-const port = 8080 || process.env.PORT
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
 
 
-app.use(cors())
+app.get('/', (req, res) => {
+  res.send("server running");
+});
 
-app.get('/',(req,res)=>{
-    res.send("server running")
-})
-
-app.listen(port,()=>{
-    console.log('server running under the ', port);
-})
+app.listen(port, () => {
+  console.log('Server running on port', port);
+});
